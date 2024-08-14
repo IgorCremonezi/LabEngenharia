cb36f588c90c4e8ea098ca185f8ef3806599bcc6
IgorCremonezi <igc2009_guilherme@hotmail.com>
2024-08-13 22:03:06 -0300

    Initial commit

---
+++ exer1.js
@@ -0,0 +1,7 @@
+let numero = prompt("Informe um número inteiro positivo: ");
+
+if (numero % 2 == 0) {
+    alert("O número informado é par!");
+} else {
+    alert("O número informado é ímpar!")
+}
